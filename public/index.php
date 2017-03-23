059cb0896ad577677cb3b92d554542a84c2c71ce
Fernando <foc364@gmail.com>
2017-03-23 15:20:29 -0300

    Incluido arquivos

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../laravel/clinica/bootstrap/autoload.php';
+require __DIR__.'/clinica/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'/../laravel/clinica/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../laravel/clinica/bootstrap/app.php';
+$app = require_once __DIR__.'/clinica/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
